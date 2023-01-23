080daabc87a56cc3c9de558701c2b0230f3b38b4
francesco <s304910@studenti.polito.it>
2023-01-23 18:46:01 +0100

    adding distance

--- RadRayCu/main.c
+++ RadRayCu/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
+#include <float.h>
 
 #define MAX_CUBE 300
 #define ENERGY_CURVE_SIZE 100
@@ -49,13 +51,13 @@ point3d rand_point(point3d min, point3d max) {
     return t;
 }
 
-float distance(point3d a, point3d b) {
+float distance(point3d a, point3d b) {  //compute distance between two points
     int dist;
     dist = sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2) + pow(a.z - b.z, 2));
     return dist;
 }
 
-point3d param_to_coord(point3d start, point3d end, float t) {
+point3d param_to_coord(point3d start, point3d end, float t) {  //return x,y of a line given its parameter t
     point3d p;
     p.x = (end.x - start.x) * t;
     p.y = (end.y - start.y) * t;
@@ -63,9 +65,9 @@ point3d param_to_coord(point3d start, point3d end, float t) {
     return p;
 }
 
-ray rand_ray(point3d min, point3d max) {
+ray rand_ray(point3d min, point3d max) {     //randomly generates the trajectory of a ray given the box bounds
     ray trajectory;
-    memcpy(LINEAR, trajectory.energy_curve);
+    memcpy(LINEAR, trajectory.energy_curve,ENERGY_CURVE_SIZE*sizeof(char));
     trajectory.start.x = rand() * (max.x - min.x) + min.x;
     trajectory.start.y = rand() * (max.y - min.y) + min.y;
     trajectory.start.z = max.z;
@@ -78,7 +80,10 @@ ray rand_ray(point3d min, point3d max) {
     return trajectory;
 }
 
-point3d* generate_points(cube curr_cube){
+void generate_points(cube curr_cube){  //generates MAX_POINTS points on a grid in each box
+    /***
+     * TODO: can be improved by generating only points in the boundary or only points near the ray.
+     * */
     int cubroot = cbrtf(MAX_POINTS);
     point3d t;
     int dx = (curr_cube.max.x - curr_cube.min.x) / cubroot;
@@ -97,20 +102,27 @@ point3d* generate_points(cube curr_cube){
         }
     }
 }
+int contains(cube curr_cube,point3d p){
+    if((p.x>=curr_cube.min.x) && (p.y>=curr_cube.min.y) && p.z>=curr_cube.min.z
+    && p.x<=curr_cube.max.x && p.y<=curr_cube.max.y && p.z<=curr_cube.max.z){
+        return 1;
+    }
+    return 0;
+}
 
-void main(int argc, char* argv[]){
+int main(int argc, char* argv[]){
     FILE* fin;
     char* inpath = "../RadrayPy/output.txt";
     int i; //i é l'indice del cubo
-    point3d CUBE_GLOBAL_MAX = {0, 0, 0}, CUBE_GLOBAL_MIN = {INT_MAX, INT_MAX, INT_MAX};
+    point3d CUBE_GLOBAL_MAX = {0, 0, 0}, CUBE_GLOBAL_MIN = {FLT_MAX, FLT_MAX, FLT_MAX};
     cube cubes[MAX_CUBE];
     cube t;
     
-    fin = fopen(inpath, "r");
+    fin = fopen(inpath, "r");  //file input, contains min and max of each cube in the GDS (2 x,y,z each)
     
-    for (i = 0; fscanf(fin, "%f,%f,%f,%f,%f,%f", &t.min.x, &t.min.y, &t.min.z, &t.max.x, &t.max.y, &t.max.z, &t.layer_n) != EOF; i++) {
+    for (i = 0; fscanf(fin, "%f,%f,%f,%f,%f,%f,%d", &t.min.x, &t.min.y, &t.min.z, &t.max.x, &t.max.y, &t.max.z, &t.layer_n) != EOF; i++) {
         cubes[i] = t;
-        if (t.max.x > CUBE_GLOBAL_MAX.x) {CUBE_GLOBAL_MAX.x = t.max.x;}
+        if (t.max.x > CUBE_GLOBAL_MAX.x) {CUBE_GLOBAL_MAX.x = t.max.x;} /* computes global max and min, in which the ray must pass */
         if (t.max.y > CUBE_GLOBAL_MAX.y) {CUBE_GLOBAL_MAX.y = t.max.y;}
         if (t.max.z > CUBE_GLOBAL_MAX.z) {CUBE_GLOBAL_MAX.z = t.max.z;}
         if (t.min.x < CUBE_GLOBAL_MIN.x) {CUBE_GLOBAL_MIN.x = t.min.x;}
@@ -121,18 +133,25 @@ void main(int argc, char* argv[]){
     
     ray ray_traj = rand_ray(CUBE_GLOBAL_MAX, CUBE_GLOBAL_MIN);
     
-    float ray_dist = distance(ray_traj.start, ray_traj.end);
+    //float ray_dist = distance(ray_traj.start, ray_traj.end);
     point3d curr_ray_p;
 
-    for(int cube_index = 0; cube_index < MAX_CUBE; cube_index++){
-        for(int point_index = 0; point_index < MAX_POINTS; point_index++){
+    for(int cube_index = 0; cube_index < MAX_CUBE; cube_index++){            //Iterates over the cubes
+        for(int point_index = 0; point_index < MAX_POINTS; point_index++){   //Iterates over points in the cube
             curr_ray_p = ray_traj.start;
-            for(int t = 0; t < N_STEPS; t++){
+            for(int t = 0; t < N_STEPS; t++){                                //Iterates over ray steps
                 curr_ray_p.x+=ray_traj.delta.x;
                 curr_ray_p.y+=ray_traj.delta.y;
                 curr_ray_p.z+=ray_traj.delta.z;
+                //check if the ray is in the cube
+                if(contains(cubes[cube_index],curr_ray_p)){
+                    int p_ray_dist= distance(cubes[cube_index].points[point_index],curr_ray_p);
+                    
+                }
             }
         }
     }
+
+    return 0;
     
 }
